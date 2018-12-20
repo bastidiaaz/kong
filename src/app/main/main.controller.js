bca175a4030b6f9b79353a48b16c02bc52e20d5e
bastidiaaz <basty.diaaz@gmail.com>
2018-12-20 19:55:11 +0100

    div mapa creado

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,5 +6,7 @@
         .controller('MainController', MainController);
 
     /** @ngInject */
-    function MainController() {}
+    function MainController() {
+      var map = L.map('mapa1').setView([51.505, -0.09], 13);
+    }
 })();
