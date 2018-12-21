fe3c4dd6a15d9a8da77ff4837f0d08896ee3aa5d
AlvaroArratia <37808434+AlvaroArratia@users.noreply.github.com>
2018-12-21 08:25:15 -0300

    leftpanel indicadores-orden

--- src/app/components/leftpanel/leftpanel.directive.js
+++ src/app/components/leftpanel/leftpanel.directive.js
@@ -28,15 +28,6 @@
             vm.maxICAP = 500;
             vm.mp25 = [vm.maxICAP, 0];
             vm.mp10 = [vm.maxICAP, 0];
-            vm.data1 = [300, 5000];
-            vm.labels2 = ["KONG", "HACKAKONG"];
-            vm.data2 = [300, 250];
-            vm.labels3 = ["Download Sales", "In-Store Sales"];
-            vm.data3 = [3000, 500];
-            vm.labels4 = ["Download Sales", "In-Store Sales"];
-            vm.data4 = [3000, 5000];
-            vm.labels5 = ["Download Sales", "HACKAKONG"];
-            vm.data5 = [300, 8000];
 
             activate();
 
